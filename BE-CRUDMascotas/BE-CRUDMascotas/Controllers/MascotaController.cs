d4aa6805a03a668ffafee86172a765bf349c29e7
Mauricio Latorre <latorre.mauricio.1992@gmail.com>
2022-10-31 19:08:29 -0300

    first commit

---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+    }
+}
